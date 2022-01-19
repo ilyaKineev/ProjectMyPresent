22eac6a5eb3f6f923d735f0ca8e03638505945d4
ilyaKineev <kineev.ilya@gmail.com>
2022-01-19 13:04:28 +0300

    new project

---
+++ src/main/java/com/ProjectMyPresent/ProjectMyPresent/ProjectMyPresentApplication.java
@@ -0,0 +1,11 @@
+package com.ProjectMyPresent.ProjectMyPresent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMyPresentApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMyPresentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ProjectMyPresent/ProjectMyPresent/ProjectMyPresentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectMyPresent.ProjectMyPresent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMyPresentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
